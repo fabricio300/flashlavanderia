a3f86d16a4b4f23aea35be822e0f68cf0c597485
MarcoMulato <marco.antonio.mulato@gmail.com>
2019-12-04 21:00:54 -0600

    cambios marco

--- src/app/paginas/resetar/resetar.page.ts
+++ src/app/paginas/resetar/resetar.page.ts
@@ -79,9 +79,9 @@ export class ResetarPage implements OnInit {
 
 randomString(length, chars) {
   var mask = '';
-  if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz0123456789';
-  if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  if (chars.indexOf('#') > -1) mask += '0123456789#?!@$%^&*-\\';
+  if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+  if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  if (chars.indexOf('#') > -1) mask += '0123456789#?!@$\\';
   if (chars.indexOf('!') > -1) mask += '#?!@$%^&*-\\';
   var result = '';
   for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
